04df1fb81adfbd5b21b0b24c0fa6313e8c8578a3
san <san@bigSan>
2014-10-07 16:52:14 +0400

    first

---
+++ mir/main.cpp
@@ -0,0 +1,22 @@
+#include "mir.h"
+#include "MirRenderer.h"
+
+#include "stdio.h"
+#include "litearray.hpp"
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+//#include "SDL2/SDL.h"
+//#include "SDL2/SDL2_gfxPrimitives.h"
+#include <random>
+
+int main(int argc, char **argv)
+{
+    int rand;
+	Mir m1(argc, argv);
+    MirRenderer mr1(&m1);
+    mr1.main();
+    return 0;
+}
---
+++ mir/main_console.cpp
@@ -0,0 +1,15 @@
+
+#include "mir.h"
+
+#include "stdio.h"
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+    int rand;
+	Mir m1(argc, argv);
+    m1.main();
+ 	return 0;
+}
